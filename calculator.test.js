d5ff34edff39f0fc44c38b5932f76d6894114113
omuriloo <murilorihan@gmail.com>
2025-05-08 16:34:38 -0300

    enviando

---
+++ calculator.test.js
@@ -0,0 +1,43 @@
+const Calculator = require('./script');
+
+describe('Calculadora', () => {
+
+  let mockDisplay;  // Mock do elemento display
+  let calc;         // Instância da calculadora
+
+  beforeEach(() => {
+    mockDisplay = { textContent: '0' };
+    
+    calc = new Calculator(mockDisplay);
+  });
+
+  test('Adição simples', () => {
+    calc.appendToDisplay('2');
+    calc.appendToDisplay('+');
+    calc.appendToDisplay('2');
+    
+    calc.calculate();
+    
+    expect(mockDisplay.textContent).toBe('4');
+  });
+
+  test('Limpar display', () => {
+    calc.appendToDisplay('1');
+    calc.appendToDisplay('2');
+    calc.appendToDisplay('3');
+    
+    calc.clearDisplay();
+    
+    expect(mockDisplay.textContent).toBe('0');
+  });
+
+  test('Multiplicação', () => {
+    calc.appendToDisplay('3');
+    calc.appendToDisplay('×');
+    calc.appendToDisplay('2');
+    
+    calc.calculate();
+    
+    expect(mockDisplay.textContent).toBe('6');
+  });
+});
